deeceb4928b289861d17d8d957e40cfa138ed64a
BhaleMaheshwar <mahesh2a6@gmail.com>
2020-04-20 23:24:18 +0530

    This is commit in branch develop by GitAPIProject guy

---
+++ src/test/java/BDD/APIRestFrameWork/Appium.java
@@ -0,0 +1,16 @@
+package BDD.APIRestFrameWork;
+
+public class Appium {
+	public void Appium1() {
+		System.out.println("Appium Testing -1");
+	}
+	
+	public void Appium2() {
+		System.out.println("Appium Testing -2");
+	}
+	public void Appium3() {
+		System.out.println("Appium Testing -3");
+	}
+	
+	
+}
---
+++ src/test/java/BDD/APIRestFrameWork/Selenium.java
@@ -0,0 +1,16 @@
+package BDD.APIRestFrameWork;
+
+public class Selenium {
+	
+	public void selenium1() {
+		System.out.println("Selenium Testing -1");
+	}
+	public void selenium2() {
+		System.out.println("Selenium Testing -2");
+	}
+	
+	public void selenium3() {
+		System.out.println("Selenium Testing -3");
+	}
+	
+}
