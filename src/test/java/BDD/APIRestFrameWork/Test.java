38e1b606e19f2029df3da20bb746112f6046a05d
BhaleMaheshwar <mahesh2a6@gmail.com>
2020-04-20 18:02:43 +0530

    This is commit from Other location guy

--- src/test/java/BDD/APIRestFrameWork/Test.java
+++ src/test/java/BDD/APIRestFrameWork/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		System.out.println("API Testing");
+		System.out.println("API Testing1");
+		System.out.println("API Testing2");
 	}
 
 }
